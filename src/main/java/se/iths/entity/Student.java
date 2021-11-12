5299e57f67b78a834c2813e70b08811485b49385
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-11-12 13:42:41 +0100

    added Student class including get date for signup date as an extra feature

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,12 +1,89 @@
 package se.iths.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
+@Entity
 public class Student {
 
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id private Long id;
+
+    @NotEmpty
+    @Size(min = 2)
     private String firstName;
+
+    @NotEmpty
+    @Size(min = 2)
     private String lastName;
+
+    @NotEmpty
     private String email;
+
+    @Size(min = 9, max = 13)
     private String phoneNumber;
+
+    private LocalDate signUpDate;
+
+    @PrePersist
+    public void getCurrentDate() {
+        setSignUpDate(LocalDate.now());
+    }
+
+    @PostPersist
+    public void itemPersisted() {
+        System.out.println("Student record added to database");
+    }
+
+    public LocalDate getSignUpDate() {
+        return signUpDate;
+    }
+
+    public void setSignUpDate(LocalDate signUpDate) {
+        this.signUpDate = signUpDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
 }
