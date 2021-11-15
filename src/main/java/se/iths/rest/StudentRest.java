48f3116923240795a12565993d612fdb4b80638f
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-11-15 09:38:28 +0100

    added validation class to split up code and make it  more presentable and understandable

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -2,6 +2,7 @@ package se.iths.rest;
 
 import se.iths.entity.Student;
 import se.iths.service.StudentService;
+import se.iths.service.Validation;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -18,60 +19,40 @@ public class StudentRest {
 
     @Inject
     StudentService studentService;
+    @Inject
+    Validation validation;
 
     @Path("create")
     @POST
     public Response createStudent(Student student) {
-        if (student.getFirstName().isEmpty() || student.getFirstName().getBytes().length < 2) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-            .entity("message: firstNameRequiredMin2Characters")
-            .type(MediaType.APPLICATION_JSON).build());
-        }
-
-        if (student.getLastName().isEmpty() || student.getLastName().getBytes().length < 2) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-            .entity("message: lastNameRequiredMin2Characters")
-            .type(MediaType.APPLICATION_JSON).build());
-        }
-
-        if (student.getEmail().isEmpty() || student.getEmail().isBlank()) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-            .entity("message: emailRequired")
-            .type(MediaType.APPLICATION_JSON).build());
-        }
-
-        studentService.createStudent(student);
-        return Response
-                .status(201)
-                .entity(student).build();
+        if (validation.validateFirstName(student) && validation.validateLastName(student) && validation.validateEmail(student)) {
+            studentService.createStudent(student);
+            return Response
+                    .status(201)
+                    .entity(student).build();
+        } else return Response.notModified().build();
     }
 
     @Path("getall")
     @GET
     public Response getAllStudents() {
-        List<Student> studentList = studentService.findAllStudents();
-        if (studentList.isEmpty()) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-            .entity("message: databaseContainsNoRecords")
-            .type(MediaType.APPLICATION_JSON).build());
-        } else
-        return Response
-                .status(200)
-                .entity(studentList).build();
+        if (validation.verifyListNotEmpty()) {
+            List<Student> studentList = studentService.findAllStudents();
+            return Response
+                    .status(200)
+                    .entity(studentList).build();
+        } else return Response.status(404).build();
     }
 
     @Path("getbyid/{id}")
     @GET
     public Response getStudentById(@PathParam("id") Long id) {
-        Student student = studentService.findStudentById(id);
-        if (student == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity("message: noRecordOfId " + id)
-                    .type(MediaType.APPLICATION_JSON).build());
-        } else
-        return Response
-                .status(200)
-                .entity(student).build();
+        if (validation.verifyStudentExists(id)) {
+            Student student = studentService.findStudentById(id);
+            return Response
+                    .status(200)
+                    .entity(student).build();
+        } else return Response.notModified().build();
     }
 
     @Path("getbylastname")
@@ -100,115 +81,67 @@ public class StudentRest {
     @Path("replace")
     @PUT
     public Response replaceStudent(Student student) {
-        if (student.getFirstName().isEmpty() || student.getFirstName().getBytes().length < 2) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-            .entity("message: firstNameInvalid")
-            .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        if (student.getLastName().isEmpty() || student.getLastName().getBytes().length < 2) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-            .entity("message: lastNameInvalid")
-            .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        if (student.getEmail().isEmpty()) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-                    .entity("message: emailInvalid")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        studentService.replaceStudent(student);
-        return Response
-                .status(202)
-                .entity(student).build();
+       if (validation.validateFirstName(student) && validation.validateLastName(student) && validation.validateEmail(student)) {
+           studentService.replaceStudent(student);
+           return Response
+                   .status(202)
+                   .entity(student).build();
+       } else return Response.notModified().build();
     }
 
     @Path("update/firstname/{id}")
     @PATCH
     public Response updateFirstName(@PathParam("id") Long id, @QueryParam("firstname") String firstName) {
-        if (studentService.findStudentById(id) == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity("message: noRecordToUpdate")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        if (firstName.isEmpty() || firstName.getBytes().length < 2) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-            .entity("message: firstNameRequiresMin2Characters")
-            .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        Student student = studentService.updateFirstName(id, firstName);
-        return Response
-                .status(202)
-                .entity(student).build();
+       if (validation.verifyStudentExists(id) && validation.validateFirstNameString(firstName)) {
+           Student student = studentService.updateFirstName(id, firstName);
+           return Response
+                   .status(202)
+                   .entity(student).build();
+       } else return Response.notModified().build();
     }
 
     @Path("update/lastname/{id}")
     @PATCH
     public Response updateLastName(@PathParam("id") Long id, @QueryParam("lastname") String lastName) {
-        if (studentService.findStudentById(id) == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity("message: noRecordToUpdate")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        if (lastName.isEmpty() || lastName.getBytes().length < 2) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-                    .entity("message: lastNameRequiresMin2Characters")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        Student student = studentService.updateLastName(id, lastName);
-        return Response
-                .status(202)
-                .entity(student).build();
+        if (validation.verifyStudentExists(id) && validation.validateLastNameString(lastName)) {
+            Student student = studentService.updateLastName(id, lastName);
+            return Response
+                    .status(202)
+                    .entity(student).build();
+        } else return Response.notModified().build();
     }
 
     @Path("update/email/{id}")
     @PATCH
     public Response updateEmail(@PathParam("id") Long id, @QueryParam("email") String email) {
-        if (studentService.findStudentById(id) == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity("message: noRecordToUpdate")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        if (email.isEmpty()) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-                    .entity("message: emailFieldIsEmpty")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        Student student = studentService.updateEmail(id, email);
-        return Response
-                .status(202)
-                .entity(student).build();
+        if (validation.verifyStudentExists(id) && validation.validateEmailString(email)) {
+            Student student = studentService.updateEmail(id, email);
+            return Response
+                    .status(202)
+                    .entity(student).build();
+        } else return Response.notModified().build();
     }
 
     @Path("update/phonenumber/{id}")
     @PATCH
     public Response updatePhoneNumber(@PathParam("id") Long id, @QueryParam("phonenumber") String phoneNumber) {
-        if (studentService.findStudentById(id) == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity("message: noRecordToUpdate")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        if (phoneNumber.isEmpty() || ((phoneNumber.getBytes().length < 9) || (phoneNumber.getBytes().length > 13))) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
-                    .entity("message: invalidPhoneNumber")
-                    .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        Student student = studentService.updatePhoneNumber(id, phoneNumber);
-        return Response
-                .status(202)
-                .entity(student).build();
+        if (validation.verifyStudentExists(id) && validation.validatePhoneNumber(phoneNumber)) {
+            Student student = studentService.updatePhoneNumber(id, phoneNumber);
+            return Response
+                    .status(202)
+                    .entity(student).build();
+        } else return Response.notModified().build();
     }
 
     @Path("delete/{id}")
     @DELETE
     public Response deleteStudent(@PathParam("id") Long id) {
-        if (studentService.findStudentById(id) == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-            .entity("message: noRecordToDelete")
-            .type(MediaType.APPLICATION_JSON_TYPE).build());
-        }
-        studentService.deleteStudent(id);
-        return Response
-                .status(202)
-                .entity("message: deletedEntryWithId" + id).build();
+        if (validation.verifyStudentExists(id)) {
+            studentService.deleteStudent(id);
+            return Response
+                    .status(202)
+                    .entity("message: deletedEntryWithId" + id).build();
+        } else return Response.notModified().build();
     }
 
 }
---
+++ src/main/java/se/iths/service/Validation.java
@@ -0,0 +1,91 @@
+package se.iths.service;
+
+import se.iths.entity.Student;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Transactional
+public class Validation {
+
+    @Inject
+    StudentService studentService;
+
+    public boolean validateFirstName(Student student) {
+        if (student.getFirstName().isEmpty() || student.getFirstName().getBytes().length < 2) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: firstNameRequiredMin2Characters")
+                    .type(MediaType.APPLICATION_JSON).build());
+        } else return true;
+    }
+
+    public boolean validateFirstNameString(String firstName) {
+        if (firstName.isEmpty() || firstName.getBytes().length < 2) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: firstNameRequiredMin2Characters")
+                    .type(MediaType.APPLICATION_JSON).build());
+        } else return true;
+    }
+
+    public boolean validateLastName(Student student) {
+        if (student.getLastName().isEmpty() || student.getLastName().getBytes().length < 2) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: lastNameRequiredMin2Characters")
+                    .type(MediaType.APPLICATION_JSON).build());
+        } else return true;
+    }
+
+    public boolean validateLastNameString(String lastName) {
+        if (lastName.isEmpty() || lastName.getBytes().length < 2) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: lastNameRequiredMin2Characters")
+                    .type(MediaType.APPLICATION_JSON).build());
+        } else return true;
+    }
+
+    public boolean validateEmail(Student student) {
+        if (student.getEmail().isEmpty() || student.getEmail().isBlank()) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: emailRequired")
+                    .type(MediaType.APPLICATION_JSON).build());
+        } else return true;
+    }
+
+    public boolean validateEmailString(String email) {
+        if (email.isEmpty()) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: emailRequired")
+                    .type(MediaType.APPLICATION_JSON).build());
+        } else return true;
+    }
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        if (phoneNumber.isEmpty() || ((phoneNumber.getBytes().length < 9) || (phoneNumber.getBytes().length > 13))) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity("message: invalidPhoneNumber")
+                    .type(MediaType.APPLICATION_JSON_TYPE).build());
+        } else return true;
+    }
+
+    public boolean verifyStudentExists(Long id) {
+        if (studentService.findStudentById(id) == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("message: noRecordForId" + id)
+                    .type(MediaType.APPLICATION_JSON_TYPE).build());
+        } else return true;
+    }
+
+    public boolean verifyListNotEmpty() {
+        List<Student> studentList = studentService.findAllStudents();
+        if (studentList.isEmpty()) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("message: databaseContainsNoRecords")
+                    .type(MediaType.APPLICATION_JSON_TYPE).build());
+        } else return true;
+    }
+
+}
